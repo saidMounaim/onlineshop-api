81cdedd01927cde71af1bb2e13e7a11a374e1e7e
Said Mounaim <saidmounaim00@gmail.com>
2021-05-22 19:39:42 +0100

    Setup Backend With Node js & Express js

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+// WELCOME ROUTE
+app.get('/api/v1/', (req, res) => {
+	res.status(201).json({ success: true, message: 'Welcome to online shop API' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
