99327aaec26de9e266cf4c1037ae7d86330db292
Said Mounaim <saidmounaim00@gmail.com>
2021-05-25 20:34:29 +0100

    Update User

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -17,3 +17,22 @@ export const addUser = asyncHandler(async (req, res) => {
 
 	res.status(201).json({ success: true, data: user });
 });
+
+//@DESC Update User
+//@ROUTE /api/v1/users/:id
+//@METHOD PUT
+export const updateUser = asyncHandler(async (req, res) => {
+	let user = await User.findById(req.params.id);
+
+	if (!user) {
+		res.status(404);
+		throw new Error('User not found');
+	}
+
+	user = await User.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+	});
+
+	res.status(201).json({ success: true, data: user });
+});
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAll, addUser } from '../controllers/UserController.js';
+import { getAll, addUser, updateUser } from '../controllers/UserController.js';
 
 const router = express.Router();
 
 router.route('/').get(getAll).post(addUser);
+router.route('/:id').put(updateUser);
 
 export default router;
