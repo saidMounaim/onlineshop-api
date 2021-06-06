152696f34c604ad215b21b7c893d7ddb2e0599f9
Said Mounaim <saidmounaim00@gmail.com>
2021-06-06 21:35:25 +0100

    Add Product Model

---
+++ backend/models/Product.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'Please add a name'],
+		},
+
+		image: {
+			type: String,
+		},
+
+		description: {
+			type: String,
+			required: [true, 'Please add a description'],
+		},
+
+		brand: {
+			type: String,
+			required: [true, 'Please add a brand'],
+		},
+
+		category: {
+			type: String,
+			required: [true, 'Please add a category'],
+			enum: ['Electronics'],
+		},
+
+		price: {
+			type: Number,
+			required: [true, 'Please add a price'],
+		},
+
+		countInStock: {
+			type: Number,
+			required: [true, 'Please add a count in stock'],
+		},
+
+		rating: {
+			comment: {
+				type: String,
+				required: [true, 'Please add a comment'],
+			},
+			rating: {
+				type: Number,
+				default: 0,
+			},
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Product = mongoose.model('Product', ProductSchema);
+
+export default Product;
