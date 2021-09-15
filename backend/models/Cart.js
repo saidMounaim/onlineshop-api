2a6b98c7fb20826a203d567648c2a421922e2d57
Said Mounaim <saidmounaim00@gmail.com>
2021-09-15 21:04:25 +0100

    Add Cart Model

---
+++ backend/models/Cart.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const CartSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+
+  products: {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Product",
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+  },
+});
+
+const Cart = mongoose.model("Cart", CartSchema);
+
+export default Cart;
